8bfb9451566fd534ce8f53cd51d0164498a93c0e
yangmr <759168442@qq.com>
2024-07-02 15:51:37 +0800

    在线选车

--- three在线选车/three-car/src/effect/MySky.js
+++ three在线选车/three-car/src/effect/MySky.js
@@ -8,7 +8,10 @@ export class MySky {
   // 初始化场景
   init() {
     // 调用创建室内展厅方法
-    this.createInDoor();
+    // this.createInDoor();
+
+    // 调用创建室外展厅方法
+    this.createOutDoor();
   }
 
   // 创建室内展厅方法
@@ -33,6 +36,45 @@ export class MySky {
     const circleMaterial = new THREE.MeshStandardMaterial({
       color: 0x42454c,
       // color: "green",
+      side: THREE.DoubleSide,
+    });
+    const circle = new THREE.Mesh(circleGeo, circleMaterial);
+    circle.rotation.set(-Math.PI / 2, 0, 0);
+    // 接受阴影
+    circle.receiveShadow = true;
+
+    this.scene.add(circle);
+  }
+
+  // 创建户外展厅方法
+  createOutDoor() {
+    // 创建图形(几何体)
+    const sphereGeo = new THREE.SphereGeometry(40, 32, 16);
+
+    // 创建纹理加载器
+    const sphereTexture = new THREE.TextureLoader().load("image/desert.jpg");
+    // 设置颜色通道为rgb
+    sphereTexture.colorSpace = THREE.SRGBColorSpace;
+    // 创建材质
+    const material = new THREE.MeshBasicMaterial({
+      map: sphereTexture,
+      side: THREE.DoubleSide,
+    });
+
+    // 创建物体
+    const sphere = new THREE.Mesh(sphereGeo, material);
+
+    // 添加到场景
+    this.scene.add(sphere);
+
+    // 创建地面
+    const circleGeo = new THREE.CircleGeometry(10, 32);
+    // 创建纹理加载器
+    const circleTexture = new THREE.TextureLoader().load("image/sand.jpg");
+    // 设置颜色通道为rgb
+    circleTexture.colorSpace = THREE.SRGBColorSpace;
+    const circleMaterial = new THREE.MeshStandardMaterial({
+      map: circleTexture,
       side: THREE.DoubleSide,
     });
     const circle = new THREE.Mesh(circleGeo, circleMaterial);
--- three在线选车/three-car/src/model/Car.js
+++ three在线选车/three-car/src/model/Car.js
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 export class MyCar {
   constructor(model, scene, camera, controls) {
     this.model = model;
@@ -5,11 +6,74 @@ export class MyCar {
     this.camera = camera;
     this.controls = controls;
 
+    // 车小模型对象
+    this.carModel = {
+      body: {
+        // 车身
+        main: {
+          name: "Object_103",
+          model: {},
+        },
+        // 车顶
+        roof: {
+          name: "Object_110",
+          model: {},
+        },
+
+        // 左车门
+        leftDoor: {
+          name: "Object_64",
+          model: {},
+        },
+        // 右车门
+        rightDoor: {
+          name: "Object_77",
+          model: {},
+        },
+
+        // 后轮胎
+        ft: {
+          name: "Object_4",
+          model: {},
+        },
+
+        // 前轮胎
+        bt: {
+          name: "Object_19",
+          model: {},
+        },
+      },
+    };
+
     this.init();
+
+    this.modifyCarDefault();
   }
 
+  // 初始化
   init() {
-    console.log("!23");
     this.scene.add(this.model);
+
+    // 匹配出所需要的模型
+    Object.values(this.carModel.body).forEach((obj) => {
+      obj.model = this.model.getObjectByName(obj.name);
+    });
+  }
+
+  // 修改匹配出的模型
+  modifyCarDefault() {
+    Object.values(this.carModel.body).forEach((obj) => {
+      obj.model.material = new THREE.MeshPhysicalMaterial({
+        color: 0xff9900,
+        // 光泽度
+        metalness: 1,
+        // 粗糙度
+        roughness: 0.5,
+        // 1. 设置清漆度（0 - 1）
+        clearcoat: 1,
+        // 2. 设置清漆度的粗糙度
+        clearcoatRoughness: 0,
+      });
+    });
   }
 }
